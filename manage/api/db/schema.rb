b51364e66d57b6eabc81fab32dfa8c45f97672f5
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-17 16:55:04 +0000

    Add：制約追加

--- manage/api/db/schema.rb
+++ manage/api/db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_17_163451) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_17_165417) do
   create_table "markers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
-    t.float "latitude"
-    t.float "longitude"
+    t.float "latitude", null: false
+    t.float "longitude", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["latitude", "longitude"], name: "index_markers_on_latitude_and_longitude", unique: true
+    t.index ["name"], name: "index_markers_on_name", unique: true
   end
 
 end
