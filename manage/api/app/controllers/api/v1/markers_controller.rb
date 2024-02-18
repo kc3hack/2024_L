74dbfc095730b2e67ef4392cee474819b7f77358
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-18 02:21:12 +0000

    Feature：マーカーデータを更新できるように(API)

--- manage/api/app/controllers/api/v1/markers_controller.rb
+++ manage/api/app/controllers/api/v1/markers_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MarkersController < Api::ApiController
-  skip_before_action :authenticate_user, only: [:create]
+  skip_before_action :authenticate_user
 
   def index
     markers = Marker.all
@@ -10,6 +10,15 @@ class Api::V1::MarkersController < Api::ApiController
     marker = Marker.new(marker_params)
     if marker.save
       render json: { status: "success", data: { marker: marker } }, status: :created
+    else
+      render json: { status: "error", data: { error: marker.errors } }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    marker = Marker.find(params[:id])
+    if marker.update(marker_params)
+      render json: { status: "success", data: { marker: marker } }, status: :ok
     else
       render json: { status: "error", data: { error: marker.errors } }, status: :unprocessable_entity
     end
