6fdcda8706f502d811b6f71814ed66d04f2666eb
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-18 10:38:15 +0000

    Add：エラーハンドリング

--- manage/api/app/controllers/api/v1/markers_controller.rb
+++ manage/api/app/controllers/api/v1/markers_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::MarkersController < Api::ApiController
   skip_before_action :authenticate_user
 
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render json: { status: "error", data: { error: error } }, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |error|
+    render json: { status: "error", data: { error: error } }, status: :unprocessable_entity
+  end
+
+  rescue_from ActionController::ParameterMissing do |error|
+    render json: { status: "error", data: { error: error } }, status: :unprocessable_entity
+  end
+
   def index
     markers = Marker.all
     render json: { status: "success", data: { markers: markers } }, status: :ok
@@ -21,7 +33,7 @@ class Api::V1::MarkersController < Api::ApiController
     if marker.save
       render json: { status: "success", data: { marker: marker } }, status: :created
     else
-      render json: { status: "error", data: { error: marker.errors } }, status: :unprocessable_entity
+      render json: { status: "error", data: { errors: marker.errors } }, status: :unprocessable_entity
     end
   end
 
@@ -30,16 +42,16 @@ class Api::V1::MarkersController < Api::ApiController
     if marker.update(marker_params)
       render json: { status: "success", data: { marker: marker } }, status: :ok
     else
-      render json: { status: "error", data: { error: marker.errors } }, status: :unprocessable_entity
+      render json: { status: "error", data: { errors: marker.errors } }, status: :unprocessable_entity
     end
   end
 
   def destroy
     marker = Marker.find(params[:id])
-    if marker.destroy
+    if marker.destroy!
       render json: { status: "success", data: { marker: marker } }, status: :ok
     else
-      render json: { status: "error", data: { error: marker.errors } }, status: :unprocessable_entity
+      render json: { status: "error", data: { errors: marker.errors } }, status: :unprocessable_entity
     end
   end
 
