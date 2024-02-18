4ecf645903bfbaa822bc3c086fa3a9b7b8674c9a
e1b21072 <e1b21072@oit.ac.jp>
2024-02-18 07:47:58 +0000

    App.tsxのフォーマット

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import Routing from "./Routing";
 
 const App = () => {
-    return (
-      <div className="App">
-        <div className="md:container mx-auto my-auto">
-          <Routing />
-        </div>
+  return (
+    <div className="App">
+      <div className="md:container mx-auto my-auto">
+        <Routing />
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 
 
