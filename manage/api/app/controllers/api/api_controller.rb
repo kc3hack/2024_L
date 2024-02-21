57704e1229b3de3ceb77677ff20cec058c483820
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-21 21:03:32 +0000

    Feature：ユーザー情報を取得できるコンテキスト作成

--- manage/api/app/controllers/api/api_controller.rb
+++ manage/api/app/controllers/api/api_controller.rb
@@ -12,10 +12,11 @@ class Api::ApiController < ApplicationController
   def authenticate_user
     token = request.headers["Authorization"]&.split(" ")&.last
     payload = FirebaseAuthenticator.new(token).validate!
-    raise AuthenticationError.new("Not Loginded") unless current_user(payload["user_id"])
+    @current_user_id = payload["user_id"]
+    raise AuthenticationError.new("Not Logged In") unless current_user
   end
 
-  def current_user(user_id)
-    @current_user ||= User.find_by(uid: user_id)
+  def current_user
+    @current_user ||= User.find_by(uid: @current_user_id) if @current_user_id
   end
 end
