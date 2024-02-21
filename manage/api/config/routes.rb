256e3b4e86826370963d365648a3449291b0cff7
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-21 19:34:47 +0000

    Feature：ユーザーデータを取得できるように(個人)

--- manage/api/config/routes.rb
+++ manage/api/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create] do
+      resources :users, only: [:show, :create] do
         resources :markers, only: [:index], to: "markers#index_by_user"
       end
       resources :markers, only: [:index, :show, :create, :update, :destroy]
