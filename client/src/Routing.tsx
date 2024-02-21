f4bd89f56ed8a7aef7a98f8e0030dce2a22668d9
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-21 16:10:44 +0000

    Feature：認証保護機能追加

--- client/src/Routing.tsx
+++ client/src/Routing.tsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+
+import { useFirebaseAuthContext } from "./providers/FirebaseAuth";
 
 import Welcome from "./pages/Welcome";
 import Maps from "./pages/Maps";
@@ -6,16 +10,27 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 
 const Routing = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/welcome" element={<Welcome />} />
-                <Route path="/maps" element={<Maps />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<SignUp />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  const location = useLocation();
+  const navigate = useNavigate();
+  const user = useFirebaseAuthContext();
+
+  // user認証が必要なページはここに追加してください。
+  const protectedRoutes = ["/", "/maps"];
+
+  useEffect(() => {
+    if (protectedRoutes.includes(location.pathname) && !user) {
+      navigate("/welcome");
+    }
+  }, [location, user, navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/maps" element={<Maps />} />
+    </Routes>
+  );
 };
 
 export default Routing;
--- client/src/providers/FirebaseAuth.tsx
+++ client/src/providers/FirebaseAuth.tsx
@@ -1,44 +1,47 @@
 import {
-    createContext,
-    useContext,
-    useEffect,
-    useState,
-    ReactNode,
-  } from "react";
-  
-  import { User, onAuthStateChanged, getAuth } from "firebase/auth";
-  
-  const FirebaseAuthContext = createContext<User | null>(null);
-  
-  const useFirebaseAuthContext = () => {
-    const context = useContext(FirebaseAuthContext);
-    if (context === undefined) {
-      throw new Error(
-        "useFirebaseAuthContextはFirebaseAuthProviderの子要素内でのみ使用できます"
-      );
-    }
-    return context;
-  };
-  
-  const FirebaseAuthProvider = ({ children }: { children: ReactNode }) => {
-    const [user, setUser] = useState<User | null>(null);
-  
-    useEffect(() => {
-      const auth = getAuth();
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        setUser(user);
-      });
-      return () => {
-        unsubscribe();
-      };
-    }, []);
-  
-    return (
-      <FirebaseAuthContext.Provider value={user}>
-        {children}
-      </FirebaseAuthContext.Provider>
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+
+import { User, onAuthStateChanged, getAuth } from "firebase/auth";
+
+const FirebaseAuthContext = createContext<User | null | undefined>(undefined);
+
+const useFirebaseAuthContext = () => {
+  const context = useContext(FirebaseAuthContext);
+  if (context === undefined) {
+    throw new Error(
+      "useFirebaseAuthContextはFirebaseAuthProviderの子要素内でのみ使用できます"
     );
-  };
-  
-  export { FirebaseAuthProvider, useFirebaseAuthContext };
-  
+  }
+  return context;
+};
+
+const FirebaseAuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null | undefined>(undefined);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (user === undefined) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <FirebaseAuthContext.Provider value={user}>
+      {children}
+    </FirebaseAuthContext.Provider>
+  );
+};
+
+export { FirebaseAuthProvider, useFirebaseAuthContext };
