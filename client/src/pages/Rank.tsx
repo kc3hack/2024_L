887b197cc9e61230c47196eb02e6138b983ce43c
maru <e1b21098@oit.ac.jp>
2024-02-22 10:04:55 +0900

    Temporary withdrawal

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,15 +1,15 @@
 const Rank = () => {
     return (
-        <div className="font-black font-mono text-center">
-            <div className="text-2xl items-center animate-tracking-in-expand">
-                あなたのランクは・・・
-            </div>
-            <div className="text-4xl items-center animate-text-flicker-in-glow">
-                ブロンズ
-            </div>
-            <div>
-                <div className="h-screen w-screen flex justify-center items-center">
-                    <img src="trophy.png"></img>
+        <div className="w-screen" style={{ backgroundImage: "url(/background.png)", backgroundSize: 'cover', width: '100%', height: '100vh', backgroundPosition: 'center' }}>
+            <div className="font-black font-mono text-center">
+                <div className="text-2xl items-center animate-tracking-in-expand">
+                    あなたのランクは・・・
+                </div>
+                <div className="text-4xl items-center animate-text-flicker-in-glow text-yellow-500">
+                    ブロンズ
+                </div>
+                <div className="flex justify-center items-center mt-8 animate-text-focus-in">
+                    <img src="trophy.png" alt="Trophy" className="w-32 h-32"></img>
                 </div>
             </div>
         </div>
