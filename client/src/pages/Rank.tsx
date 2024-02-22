180d43856fa4b713adb2d3915c441678ae8e7b60
maru <e1b21098@oit.ac.jp>
2024-02-22 09:09:22 +0900

    Temporary withdrawal

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,14 +1,15 @@
 const Rank = () => {
     return (
-        <div className="text-center">
+        <div className="font-black font-mono text-center">
+            <div className="text-2xl items-center animate-tracking-in-expand">
+                あなたのランクは・・・
+            </div>
+            <div className="text-4xl items-center animate-text-flicker-in-glow">
+                ブロンズ
+            </div>
             <div>
                 <div className="h-screen w-screen flex justify-center items-center">
                     <img src="trophy.png"></img>
-                </div>
-                <div className="absolute bottom-0 ">
-                    <div className="fixed bottom-16 right-0 py-5 px-0">
-                        <img src="trophy.png" width={100} height={100} />
-                    </div>
                 </div>
             </div>
         </div>
