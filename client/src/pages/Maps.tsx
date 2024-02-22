4eef02fc6dd9f44e28c4c1e005c5d3432f83b2ca
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-22 20:20:13 +0900

    feat: ユーザのピン取得状況の取得機能の追加

--- client/src/pages/Maps.tsx
+++ client/src/pages/Maps.tsx
@@ -1,104 +1,144 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { getAuth } from 'firebase/auth';
-import { useNavigate } from "react-router-dom";
-import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps"
-import { Radio, RadioGroup, FormControlLabel } from "@mui/material";
-import { API_URL } from '../config';
+import React, {useEffect, useState} from "react";
+import {APIProvider, Map, AdvancedMarker, Pin, InfoWindow} from "@vis.gl/react-google-maps"
+import {Radio, RadioGroup, FormControlLabel} from "@mui/material";
 import axios from "axios";
+import {useAPIUserData} from "@/providers/APIUserData";
+import {GOOGLE_MAP_ID, GOOGLE_MAP_KEY, API_URL} from "../config";
 
+//axiosのインスタンスを作成
 const api = axios.create({
-    baseURL: API_URL
+    baseURL: API_URL,
 });
 
+const containerStyle = {
+    width: '90%',
+    height: '60vh'
+};
 
+const center = {
+    lat: 34.841928,
+    lng: 135.705585
+};
+
+type Marker = {
+    id: number,
+    name: string,
+    description: string,
+    latitude: number,
+    longitude: number,
+    created_at: string,
+    updated_at: string,
+    address: string,
+    point: number,
+};
+
+const getMarkers = async () => {
+    const response = await api.get("/api/v1/markers");
+    return response.data.data.markers;
+};
+
+const getUserMarkers = async (userId: number) => {
+    const response = await api.get(`/api/v1/users/${userId}/markers`);
+    return response.data.data.markers;
+};
+
+/**
+ * マーカーを取得
+ */
 const Maps = () => {
-    const API = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-    const mapId = `${process.env.REACT_APP_GOOGLE_MAPS_ID}`;
-    const [locations, setLocations] = useState<{
-        id: number,
-        name: string,
-        description: string,
-        latitude: number,
-        longitude: number,
-        created_at: string,
-        update_at: string,
-        address: string,
-        point: number,
-    }[]>([]);
+    const [markers, setMarkers] = useState<Marker[]>([]);
+    const [userMarkers, setUserMarkers] = useState<Marker[]>([]);
+    const [selectedMarkers, setSelectedMarkers] = useState<Marker[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const apiUser = useAPIUserData();
+    const [selected, setSelected] = useState<"all" | "notreached">("all");
 
-    const containerStyle = {
-        width: '90%',
-        height: '60vh'
-    };
-
-    const center = {
-        lat: 34.841928,
-        lng: 135.705585
-    };
-
+    // マーカーを取得
     useEffect(() => {
-        const apiGet = async () => {
-            const responce = await api.get('/api/v1/markers');
-            const locations_api = responce.data.data.markers;
-            setLocations(locations_api);
-        };
-        apiGet();
-        setIsLoading(false);
+        getMarkers().then((markers) => {
+            setMarkers(markers);
+            setSelectedMarkers(markers);
+            setIsLoading(false);
+        });
     }, []);
 
+    //ユーザが取得済みのマーカーを取得
+    useEffect(() => {
+        if (!apiUser) return;
+        getUserMarkers(apiUser.id).then(setUserMarkers);
+    }, [apiUser]);
 
+    useEffect(() => {
+        setSelectedMarkers(selected === "all"
+            ? markers
+            : markers.filter(marker => !userMarkers.some(userMarker => userMarker.id === marker.id))
+        );
+    }, [selected, markers, userMarkers]);
 
-    const [open, setOpen] = useState(Array(locations.length).fill(false));
-    const changeOpenState = (index: number) => {
-        const newOpenState = [...open];
-        newOpenState[index] = !newOpenState[index];
-        setOpen(newOpenState);
-    };
-
-    const [selected, setSelected] = useState("all");
-    const changeValue = (event: React.ChangeEvent<HTMLInputElement>) => setSelected((event.target as HTMLInputElement).value);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     return (
-        <div className="w-screen" style={{ backgroundImage: "url(/home_bg.jpg)", backgroundSize: 'cover', width: '100%', height: '100vh', backgroundPosition: 'center' }}>
+        <div className="w-screen" style={{
+            backgroundImage: "url(/home_bg.jpg)",
+            backgroundSize: 'cover',
+            width: '100%',
+            height: '100vh',
+            backgroundPosition: 'center'
+        }}>
             <div className="flex flex-col items-center justify-center h-screen">
-                {isLoading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <APIProvider apiKey={API}>
-                        <Map defaultCenter={center} mapId={mapId} style={containerStyle} defaultZoom={18} >
-                            {Array.isArray(locations) && locations.map((location, index) => {
-                                const lat = location.latitude;
-                                const lng = location.longitude;
-                                return (
-                                    <AdvancedMarker key={index} position={{ lat, lng }} onClick={() => changeOpenState(index)}>
-                                        <Pin background={"blue"} borderColor={"white"} glyphColor={"white"} />
-                                        {open[index] && <InfoWindow key={index} position={{ lat, lng }} onCloseClick={() => changeOpenState(index)}>
-                                            <div className="leading-loose bg-transparent">
-                                                {location.name}
-                                                {location.description}
-                                                <p className="underline shadow-lg">
-                                                    {location.point}ポイント
-                                                </p>
-                                            </div>
-                                        </InfoWindow>}
-                                    </AdvancedMarker>
-                                );
-
-                            })}
-                        </Map>
-                    </APIProvider>
-                )}
-
-
-                <div className="form-check">
-                    <RadioGroup defaultValue={"all"} onChange={changeValue}>
-                        <FormControlLabel value={"all"} control={<Radio />} label="すべて表示" />
-                        <FormControlLabel value={"notreached"} control={<Radio />} label="未到達のみ表示" />
-                    </RadioGroup>
-                </div>
+                {GOOGLE_MAP_KEY && <APIProvider apiKey={GOOGLE_MAP_KEY}>
+                    <Map defaultCenter={center} mapId={GOOGLE_MAP_ID} style={containerStyle} defaultZoom={18}>
+                        {selectedMarkers.map((marker, index) => (
+                            <MarkerComponent key={index} marker={marker}/>
+                        ))}
+                    </Map>
+                </APIProvider>}
+                <MarkerFilter selected={selected} setSelected={setSelected}/>
             </div>
         </div>
     );
 };
+
+type MarkerComponentProps = { marker: Marker };
+const MarkerComponent: React.FC<MarkerComponentProps> = ({marker}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <AdvancedMarker position={{lat: marker.latitude, lng: marker.longitude}} onClick={() => setIsOpen(!isOpen)}>
+            <Pin background={"blue"} borderColor={"white"} glyphColor={"white"}/>
+            {isOpen && <InfoWindow position={{lat: marker.latitude, lng: marker.longitude}}
+                                   onCloseClick={() => setIsOpen(!isOpen)}>
+                <div className="leading-loose bg-transparent">
+                    <p>{marker.name}</p>
+                    <p>{marker.description}</p>
+                    <p className="underline shadow-lg">
+                        {marker.point}ポイント
+                    </p>
+                </div>
+            </InfoWindow>}
+        </AdvancedMarker>
+    );
+};
+
+type SelectedOption = "all" | "notreached";
+
+type MarkerFilterProps = {
+    selected: SelectedOption;
+    setSelected: React.Dispatch<React.SetStateAction<SelectedOption>>;
+};
+const MarkerFilter: React.FC<MarkerFilterProps> = ({setSelected}) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setSelected(event.target.value as SelectedOption);
+
+    return (
+        <div className="form-check">
+            <RadioGroup defaultValue={"all"} onChange={handleChange}>
+                <FormControlLabel value={"all"} control={<Radio/>} label="すべて表示"/>
+                <FormControlLabel value={"notreached"} control={<Radio/>} label="未到達のみ表示"/>
+            </RadioGroup>
+        </div>
+    );
+};
+
 export default Maps;
