e40284c68b24a6a3f2e50feb174540c36f8b8313
maru <e1b21098@oit.ac.jp>
2024-02-24 00:00:13 +0900

    動作確認できていない

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,6 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAPIUserData } from '@/providers/APIUserData';
+import { API_URL } from '@/config';
+
+const api = axios.create({
+    baseURL: API_URL
+});
+
 const Rank = () => {
+    const userData = useAPIUserData();
+    const [gradesPoint, setGradesPoint] = useState(0); // 初期値は0
+    const [gradesName, setGradesName] = useState("Bronze"); // 初期値はBronze
+
+    useEffect(() => {
+        if (userData) {
+            if (userData.point < 100) {
+                setGradesName("Bronze");
+                setGradesPoint(100);
+            } else if (100 <= userData.point && userData.point < 200) {
+                setGradesName("Silver");
+                setGradesPoint(200);
+            } else {
+                setGradesName("Gold");
+                setGradesPoint(300);
+            }
+        }
+    }, [userData]); // userData.pointが変更されたら再実行
+
+    if (!userData) return <div>loading...</div>;
+
     const gradientProgressBarStyle = {
-        width: '85%', // (現在値/全体値)×100 ％で指定してねー
+        width: `${(userData.point / gradesPoint) * 100}%`,
         backgroundImage: 'linear-gradient(to right, #ff5623, #d3ff42)',
     };
 
@@ -11,13 +41,12 @@ const Rank = () => {
                     あなたのランクは・・・
                 </div>
                 <div className="text-4xl items-center animate-text-flicker-in-glow text-yellow-500">
-                    ブロンズ
+                    {gradesName}
                 </div>
                 <div className="flex justify-center items-center mt-8 animate-text-focus-in">
                     <img src="trophy.png" alt="Trophy" className="w-32 h-32"></img>
                 </div>
-                あつめた関西ポイント：1000pt
-
+                あつめた関西ポイント：{userData.point}pt/{gradesPoint}pt
                 <div className="example" style={{ border: '1px solid #333', background: '#999', width: '200px', margin: '10px auto 0' }}>
                     <div style={{ height: '15px', ...gradientProgressBarStyle }}></div>
                 </div>
