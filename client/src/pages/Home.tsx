b15445d1b0264df17317ef3f26d97466c1a63dd3
e1b21072 <e1b21072@oit.ac.jp>
2024-02-24 10:29:33 +0000

    ホーム画面のトロフィーにlinkを追加

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -19,7 +19,9 @@ const Home = () => {
           </div>
           <div className="absolute bottom-0 ">
             <div className="fixed bottom-16 right-0 py-5 px-0">
-              <img src="trophy.png" width={100} height={100} />
+              <a href="/rank">
+                <img src="trophy.png" width={100} height={100} />
+              </a>
             </div>
           </div>
         </div>
